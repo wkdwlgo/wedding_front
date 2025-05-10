1460083e41b2e9b32a3e983c2f6abe8c6472ed0e
지솔레일 <janggh1012@gmail.com>
2025-05-10 23:02:55 +0900

    [Feat] : guestbook 모달 submit 버튼 클릭시 handleError toast 띄우기

--- src/components/GuestBookWriteModal.tsx
+++ src/components/GuestBookWriteModal.tsx
@@ -3,9 +3,11 @@
 import { useForm } from 'react-hook-form';
 
 import Modal from '@/components/@shared/Modal';
+import useToast from '@/hooks/useToast';
 
 import Input from './@shared/Input';
 import TextArea from './@shared/TextArea';
+import Toast from './@shared/Toast';
 interface GuestBookWriteModalProps {
   isModal: boolean;
   setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -27,9 +29,13 @@ export default function GuestBookWriteModal({
     reset,
   } = useForm<FormData>();
 
+  const { toastVisible, toastMessage, toastType, handleError } = useToast();
+
   const onSubmit = (data: FormData) => {
     console.log(data);
     reset();
+    handleError('API 배포가 되지 않았습니다.');
+    //handleSuccess('메시지 남기기 성공했습니다!');
     setIsModal(false);
   };
 
@@ -39,62 +45,65 @@ export default function GuestBookWriteModal({
   };
 
   return (
-    <Modal
-      isOpen={isModal}
-      onClose={closeModalhandler}
-      customDimStyle="w-[300px] md:w-[400px] "
-    >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-5 text-center"
+    <>
+      <Modal
+        isOpen={isModal}
+        onClose={closeModalhandler}
+        customDimStyle="w-[300px] md:w-[400px] "
       >
-        <p className="font-semibold text-text">작성하기</p>
-        <Input
-          variant="default"
-          fontSize="14"
-          gap="8"
-          placeholder="이름"
-          errorMessage={
-            errors.name?.type === 'maxLength'
-              ? '이름은 10글자 이내로 설정하셔야 합니다.'
-              : ''
-          }
-          isError={!!errors.name}
-          inputProps={{
-            ...register('name', { required: true, maxLength: 10 }),
-          }}
-        />
-        <TextArea
-          size="small"
-          variant="light"
-          placeholder="축하메시지"
-          errorMessage={
-            errors.message?.type === 'maxLength'
-              ? '축하메시지는 500자 이내로 써주셔야 합니다.'
-              : ''
-          }
-          isError={!!errors.message}
-          inputProps={{
-            ...register('message', { required: true, maxLength: 500 }),
-          }}
-        />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 text-center"
+        >
+          <p className="font-semibold text-text">작성하기</p>
+          <Input
+            variant="default"
+            fontSize="14"
+            gap="8"
+            placeholder="이름"
+            errorMessage={
+              errors.name?.type === 'maxLength'
+                ? '이름은 10글자 이내로 설정하셔야 합니다.'
+                : ''
+            }
+            isError={!!errors.name}
+            inputProps={{
+              ...register('name', { required: true, maxLength: 10 }),
+            }}
+          />
+          <TextArea
+            size="small"
+            variant="light"
+            placeholder="축하메시지"
+            errorMessage={
+              errors.message?.type === 'maxLength'
+                ? '축하메시지는 500자 이내로 써주셔야 합니다.'
+                : ''
+            }
+            isError={!!errors.message}
+            inputProps={{
+              ...register('message', { required: true, maxLength: 500 }),
+            }}
+          />
 
-        <div className="flex items-center justify-center gap-5">
-          <button
-            type="submit"
-            className="w-[100%] rounded-full bg-text px-4 py-3 text-sm text-white2"
-          >
-            작성하기
-          </button>
-          <button
-            type="button"
-            onClick={closeModalhandler}
-            className="w-[100%] rounded-full bg-text px-4 py-3 text-sm text-white2"
-          >
-            취소하기
-          </button>
-        </div>
-      </form>
-    </Modal>
+          <div className="flex items-center justify-center gap-5">
+            <button
+              type="submit"
+              className="w-[100%] rounded-full bg-text px-4 py-3 text-sm text-white2"
+            >
+              작성하기
+            </button>
+            <button
+              type="button"
+              onClick={closeModalhandler}
+              className="w-[100%] rounded-full bg-text px-4 py-3 text-sm text-white2"
+            >
+              취소하기
+            </button>
+          </div>
+        </form>
+      </Modal>
+      {toastVisible && <Toast message={toastMessage} type={toastType} />}
+    </>
   );
 }
