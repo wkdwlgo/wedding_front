d1e49742f2b0134a7287786373f400cb70cc44af
지솔레일 <janggh1012@gmail.com>
2025-05-10 23:04:04 +0900

    [Design] : checkbox.svg 색상 변경 및 Toast 레이아웃 변경

--- src/components/@shared/Toast.tsx
+++ src/components/@shared/Toast.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import Image from 'next/image';
-import React from 'react';
 
 interface ToastProps {
   message: string;
@@ -9,28 +8,28 @@ interface ToastProps {
 
 export default function Toast({ message, type }: ToastProps) {
   const textColor = clsx({
-    'text-white': type === 'success',
+    'text-text': type === 'success',
     'text-status-danger': type === 'error',
   });
 
   return (
     <aside
       role="alert"
-      className="border-primary-5 bg-default-tertiary fixed bottom-28 left-1/2 z-100 w-11/12 -translate-x-1/2 translate-y-full transform rounded-lg border-2 p-4 shadow-lg transition-transform duration-300 ease-in-out md:w-fit"
+      className="fixed bottom-28 left-1/2 z-100 w-11/12 -translate-x-1/2 translate-y-full transform rounded-lg border-2 border-text bg-white2 p-4 shadow-lg transition-transform duration-300 ease-in-out md:w-fit"
     >
       <div className="flex justify-center gap-2">
         {type === 'success' ? (
           <Image
             src={'/images/icons/checkbox.svg'}
-            width={10}
-            height={10}
+            width={23}
+            height={23}
             alt="success"
           />
         ) : (
           <Image
             src={'/images/icons/warning.svg'}
-            width={10}
-            height={10}
+            width={23}
+            height={23}
             alt="error"
           />
         )}
